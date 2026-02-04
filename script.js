f4f474c6924772803e5a7e1148a00c9b787e89f4
NicolleValovicova <302245247@student.rocmondriaan.nl>
2026-02-04 01:54:36 +0100

    added eventlisteners, working on duration time

--- script.js
+++ script.js
@@ -4,3 +4,49 @@ let endTime = null;
 let backspaceCount = 0;
 let keystrokes = [];
 
+let typingArea = document.querySelector("#typingArea");
+let allStats = document.querySelectorAll(".stats");
+// buttons
+const startBtn = document.querySelector("#startBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+typingArea.addEventListener("keydown", function (event) {
+  if (sessionActive === false) {
+    return;
+  } else{
+
+      let keyObject = { key: event.key, time: Date.now(), type: event.code };
+      console.log(event.key);
+      console.log(event.code);
+      keystrokes.push(keyObject);      
+      if ((keyObject.key === "Backspace")) {
+        backspaceCount++;
+        console.log(backspaceCount);
+      }
+  }
+  
+});
+console.log(keystrokes);
+
+startBtn.addEventListener("click", () => {
+  sessionActive = true;
+  startTime = Date.now();
+  endTime = null;
+  typingArea.value = "";
+  keystrokes = [];
+  backspaceCount = 0;
+});
+
+resetBtn.addEventListener("click", () => {
+  allStats.forEach((stat) => {
+    stat.textContent = "";
+  });
+  endTime = Date.now();
+  sessionActive = false;
+  startTime = null;
+  backspaceCount = 0;
+});
+
+function getDurationSeconds(){
+    
+}
