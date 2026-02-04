d5d6394934265ce703e1162c522a88ccc8bce2d2
NicolleValovicova <302245247@student.rocmondriaan.nl>
2026-02-04 16:09:56 +0100

    added a targetsentence fixing bugs

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ let backspaceCount = 0;
 let keystrokes = [];
 
 let typingArea = document.querySelector("#typingArea");
+  let typeValue = typingArea.value;
+
 let allStats = document.querySelectorAll(".stats");
 let statWpm = document.querySelector("#wpm");
 let accuracy = document.querySelector("#accuracy");
 let errors = document.querySelector("#errors");
 let backspaces = document.querySelector("#backspaces");
 let pauseAvg = document.querySelector("#pauseAvg");
+
+let targetText = document.querySelector("#targetText");
+let targetTextLength = targetText.value;
 // buttons
 const startBtn = document.querySelector("#startBtn");
 const resetBtn = document.querySelector("#resetBtn");
@@ -28,6 +33,7 @@ typingArea.addEventListener("keydown", function (event) {
       backspaces.textContent = `Backspaces: ${backspaceCount}`;
     }
   }
+  updateStats();
 });
 console.log(keystrokes);
 
@@ -64,33 +70,49 @@ function calculateWPM(text, seconds) {
   let words = text.trim().split(/\s+/).length;
   let minutes = seconds / 60;
   let wpm = words / minutes;
-  statWpm.textContent = `PauseAvg: ${Math.floor(wpm)}`;
+  statWpm.textContent = `WPM: ${Math.floor(wpm)}`;
   return Math.floor(wpm);
 }
 
-function calculateErrors(typedText, targetText) {
-  errors = 0;
+function calculateErrors(typedText, targetTextLength) {
+  let calcErrors = 0;
   for (let i = 0; i < keystrokes.length; i++) {
     const stroke = keystrokes[i];
-    for (let a = 0; a < targetText.length; a++) {
-      const element = targetText[a];
+    for (let a = 0; a < targetTextLength.length; a++) {
+      const element = targetTextLength[a];
       if (stroke !== element) {
         console.log("wrong letter");
       }
     }
-    if (typedText.length > targetText.length) {
-      errors = typedText.length - targetText.length;
+    if (typedText.length > targetTextLength.length) {
+      calcErrors = typedText.length - targetTextLength.length;
+      errors.textContent = `Error: ${calcErrors}`;
       return;
     }
   }
 }
 
 function calculateAccuracy(errors, typedLength) {
-  let calcedAccuracy = (1 - errors / typedLength) * 100;
-  accuracy.textContent = `Accuracy: ${Math.floor(calcedAccuracy)}`
-  return Math.floor(calcedAccuracy);
+  let calcedAccuracy = Math.floor((1 - errors / typedLength) * 100);
+  accuracy.textContent = `Accuracy: ${calcedAccuracy}`;
+  return calcedAccuracy;
 }
 
-function calculateAveragePause(keystrokes){
-    
+function calculateAveragePause(keystrokes) {
+  pauseAvg = 0;
+  for (let i = 0; i < keystrokes.length; i++) {
+    const stroke = keystrokes[i];
+    let betwheentime = (stroke.time + stroke.time) / keystrokes.length;
+    pauseAvg += betwheentime;
+    pauseAvg.textContent = `PauseAvg: ${pauseAvg}`;
+    return pauseAvg;
+  }
+}
+
+function updateStats() {
+  let totalDuration = getDurationSeconds();
+  calculateWPM(typeValue, totalDuration);
+ let calculatedErrors = calculateErrors(typeValue, targetText);
+  calculateAccuracy(calculatedErrors, typeValue.length);
+  calculateAveragePause(keystrokes);
 }
